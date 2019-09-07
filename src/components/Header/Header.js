e08c5327439721a13472fd3c4ca90ef3756cb905
First Last <salmanashraf853@gmail.com>
2019-09-07 10:10:47 +0100

    starting with the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,17 @@ import './head.css'
 
 const Header = () => {
     return (
-        <div className="header">header</div>
+        <div className="header">
+            <div className="logo">
+                <img src={require('./logo.png')}/>
+            </div>
+            <div className="search-box">
+                search
+            </div>
+            <div className="likes">
+                likes
+            </div>
+        </div>
     )
 }
 export default Header
