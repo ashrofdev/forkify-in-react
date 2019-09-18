203138fb77662cbed34fc362914c047dc611f61e
First Last <salmanashraf853@gmail.com>
2019-09-18 16:06:33 +0100

    Updates

---
+++ precache-manifest.83081ca666c2473404b4d562f66bc65e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071155d03f589e3adcb2802d0a8d3555",
+    "url": "/forkify-in-react/index.html"
+  },
+  {
+    "revision": "a9d49cc5da4fa2af50e0",
+    "url": "/forkify-in-react/static/css/2.87e65379.chunk.css"
+  },
+  {
+    "revision": "bf460026ac22dc5295f3",
+    "url": "/forkify-in-react/static/css/main.a1199cec.chunk.css"
+  },
+  {
+    "revision": "a9d49cc5da4fa2af50e0",
+    "url": "/forkify-in-react/static/js/2.1e3282b5.chunk.js"
+  },
+  {
+    "revision": "bf460026ac22dc5295f3",
+    "url": "/forkify-in-react/static/js/main.33d0f41d.chunk.js"
+  },
+  {
+    "revision": "d01fcd7f2b409a0e28bd",
+    "url": "/forkify-in-react/static/js/runtime~main.c72a1814.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/forkify-in-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/forkify-in-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/forkify-in-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/forkify-in-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/forkify-in-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "31e2931212400bb633d74a8c09084f39",
+    "url": "/forkify-in-react/static/media/logo.31e29312.png"
+  },
+  {
+    "revision": "8adff87568df68c6a2a89d8db5cae6ad",
+    "url": "/forkify-in-react/static/media/spinner.8adff875.png"
+  }
+]);
